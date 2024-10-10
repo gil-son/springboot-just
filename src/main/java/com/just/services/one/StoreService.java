f424c45550d8f4089ed2a6d09a9c4672a38c08b5
gil-son <gilszon@gmail.com>
2024-10-09 22:20:29 -0300

    Services

--- src/main/java/com/just/services/one/StoreService.java
+++ src/main/java/com/just/services/one/StoreService.java
@@ -1,4 +1,13 @@
 package com.just.services.one;
 
+import com.just.entities.one.Store;
+import com.just.repositories.StoreRepository;
+
 public class StoreService {
+
+    private StoreRepository storeRepository;
+
+    public Store saveStore(Store store) {
+        return storeRepository.save(store);
+    }
 }
