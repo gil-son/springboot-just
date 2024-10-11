9d825c17c7850233e293245b2d43db96552bbad1
gil-son <gilszon@gmail.com>
2024-10-10 21:48:46 -0300

    Splited JSON and saved each part

--- src/main/java/com/just/controllers/one/StoreController.java
+++ src/main/java/com/just/controllers/one/StoreController.java
@@ -1,5 +1,6 @@
 package com.just.controllers.one;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.just.entities.one.Store;
 import com.just.services.one.StoreService;
 import org.slf4j.Logger;
@@ -8,21 +9,45 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
+@RestController
+@RequestMapping(value = "/store")
 public class StoreController {
 
     private static final Logger logger = LoggerFactory.getLogger(Store.class);
 
     private final StoreService storeService;
 
-    @Autowired
     public StoreController(StoreService storeService) {
         this.storeService = storeService;
     }
 
-    @PostMapping
-    public ResponseEntity<Store> createStore(@RequestBody Store store) {
+    @PostMapping(value = "/mapperless")
+    public ResponseEntity<Store> createStoreMapperLess(@RequestBody Store store) {
         Store savedStore = storeService.saveStore(store);
         return ResponseEntity.ok(savedStore);
     }
+
+    @PostMapping(value = "/mapper")
+    public ResponseEntity<Store> createStoreUsingMapper(@RequestBody Map<String, Object> storeData) {
+
+        logger.info("StoreData: {}", storeData);
+
+        // Extract the nested store object from the JSON
+        Map<String, Object> storeMap = (Map<String, Object>) storeData.get("store");
+
+        // Manually map the JSON structure to a Store entity
+        ObjectMapper objectMapper = new ObjectMapper();
+        Store store = objectMapper.convertValue(storeMap, Store.class);
+
+        logger.info("Store: {}", store);
+
+        Store savedStore = storeService.saveStore(store);
+        return ResponseEntity.ok(savedStore);
+    }
+
 }
--- src/main/java/com/just/services/one/StoreService.java
+++ src/main/java/com/just/services/one/StoreService.java
@@ -2,9 +2,13 @@ package com.just.services.one;
 
 import com.just.entities.one.Store;
 import com.just.repositories.StoreRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class StoreService {
 
+    @Autowired
     private StoreRepository storeRepository;
 
     public Store saveStore(Store store) {
